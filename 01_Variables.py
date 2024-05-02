48a9b9c032be21ec50095077c081f964e4d74bb5
sanuv9683 <sanuv9683@gmail.com>
2024-05-02 15:36:31 +0100

    Content Updated

---
+++ 01_Variables.py
@@ -0,0 +1,20 @@
+age=20
+name='Dasun'
+height=5.7
+have_a_car=True
+#like this we can make variables in python
+
+#type conversions
+int("10")
+float('10.1')
+str(19)
+state=bool("True")
+print(state)
+
+#the type String
+my_name='Ramal Wasuka'
+my_name.capitalize()
+
+#get keyboard input
+my_First_Name=input("Input Your First Name : ")
+print(my_First_Name)
